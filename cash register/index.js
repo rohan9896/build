09a8840a772351fdc36be0fd0fdab13289f8c6c9
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2020-12-25 02:23:08 +0530

    refractor

---
+++ cash register/index.js
@@ -0,0 +1,11 @@
+var notesArray = [1, 5, 10, 20, 100, 500, 2000];
+
+var greatestNoteSmallerThanAmount = (amount) => {
+    notesArray.forEach((item, index) => {
+        if(item > amount){
+            
+        }
+    });
+}
+
+console.log(greatestNoteSmallerThanAmount(1123));
