fee8d9438e1a4e35636b6bbcb503641d67069709
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-01-28 01:22:42 +0530

    js part done

--- cash register/cashRegister.js
+++ cash register/cashRegister.js
@@ -1,23 +1,43 @@
+var billAmount = document.getElementById("bill-amount");
+var amountReceived = document.getElementById("amount-received");
+var btn = document.getElementById("cal-btn");
+var message = document.getElementById("message");
+
 var notesArray = [2000, 500, 100, 20, 10, 5, 1];
-var notesToBeReturned = [];
+var notesFreqMap = new Map();
 
-var greatestNoteSmallerThanAmount = (amountRecieved, billAmount) => {
+var calculate = (balance, notesArray) => {
+  for (let i = 0; i < notesArray.length; i++) {
+    let freqOfThisNote = Math.floor(balance / notesArray[i]);
+    notesFreqMap.set(notesArray[i], freqOfThisNote);
+    balance -= notesArray[i] * freqOfThisNote;
 
-    if(amountRecieved < billAmount){
-        console.log("Customer needs to give "+(billAmount-amountRecieved)+"more!");
-    }
-    else if(amountRecieved === billAmount){
-        console.log("Customer has given exactly same amount!")
-    }
-    else{
-        let balanceAmount = amountRecieved - billAmount;
-        notesArray.forEach((note) => {
-            if (balance !== 0) {
-                let numberOfNotes = Math.floor(balanceAmount / note);
-                balanceAmount = balanceAmount - numberOfNotes * note;
-            }
-        });
-    }
-}
+    if (balance == 0) break;
+  }
+};
 
-greatestNoteSmallerThanAmount(3000, 700);
+var billAmountVal = billAmount.value;
+var amountReceivedVal = amountReceived.value;
+
+var clickHandler = () => {
+  var balance = amountReceived - billAmountVal;
+
+  if (balance === 0) {
+    message.innerText = "The customer has given exact amount!!";
+  } else if (balance < 0) {
+    message.innerText =
+      "User has given " + -1 * balance + " less than bill amount!";
+  } else {
+    calculate(balance, notesArray);
+    var messageString = "";
+    for (let entry of notesFreqMap) {
+      if (entry[1] > 0) {
+        console.log(entry[0]+": "+entry[1]);
+        messageString.append(entry[0] + ": " + entry[1]);
+      }
+    }
+    // message.innerText = messageString;
+  }
+};
+
+btn.addEventListener("click", clickHandler);
